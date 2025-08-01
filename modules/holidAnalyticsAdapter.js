ec1ea12f2c14a3af95c5c7f0f65abcea78fb08c7
samumaretiyah <sam.umaretiya@holid.se>
2025-08-01 11:56:21 +0530

    added GDPR

--- modules/holidAnalyticsAdapter.js
+++ modules/holidAnalyticsAdapter.js
@@ -4,81 +4,199 @@ import { ajax } from '../src/ajax.js';
 import { logInfo } from '../src/utils.js';
 import { EVENTS } from '../src/constants.js';
 
-const ANALYTICS_URL = 'https://analytics.holid.io/prebid.php';
+const ANALYTICS_URL = 'https://analytics.holid.io/prebid/';
+const END_POINT = "holid.php";
 
-const holidAnalytics = Object.assign(adapter({url: ANALYTICS_URL, analyticsType: 'endpoint'}), {
+let holidAnalytics = Object.assign(adapter({url: ANALYTICS_URL, analyticsType: 'endpoint'}), {
   context: {},
+
   track({eventType, args}) {
-    switch (eventType) {
-      case EVENTS.AUCTION_INIT:
-        this.context.auctionId = args.auctionId;
-        this.context.startTime = args.timestamp;
-        break;
+      switch (eventType) {
+        case EVENTS.AUCTION_INIT:
+          logInfo('HOLID_AUCTION_INIT:', args);
+          this.context[args.auctionId] = {
+            auctionId: args.auctionId,
+            startTime: args.timestamp,
+            bidRequests: [],
+            bidResponses: [],
+            bidRejected: [],
+            bidderDone: [],
+            bidTimeout: [],
+            winningBids: [],
+            adRenderFailed: []
+          };
+          break;
 
-      case EVENTS.BID_REQUESTED:
-        this.context.bidRequests = this.context.bidRequests || [];
-        this.context.bidRequests.push(args);
-        break;
+        case EVENTS.BID_REQUESTED:
+          logInfo('HOLID_BID_REQUESTED:', args);
+          if (this.context[args.auctionId]) {
+            this.context[args.auctionId].bidRequests.push(args);
+          }
+          break;
 
-      case EVENTS.BID_RESPONSE:
-        this.context.bidResponses = this.context.bidResponses || [];
-        this.context.bidResponses.push(args);
-        break;
+        case EVENTS.BID_RESPONSE:
+          logInfo('HOLID_BID_RESPONSE:', args);
+          if (this.context[args.auctionId]) {
+            this.context[args.auctionId].bidResponses.push(args);
+          }
+          break;
 
-      case EVENTS.BID_REJECTED:
-        this.context.bidRejected = this.context.bidRejected || [];
-        this.context.bidRejected.push(args);
-        break;
+        case EVENTS.BID_REJECTED:
+          logInfo('HOLID_BID_REJECTED:', args);
+          if (this.context[args.auctionId]) {
+            this.context[args.auctionId].bidRejected.push(args);
+          }
+          break;
 
-      case EVENTS.BIDDER_DONE:
-        this.context.bidderDone = this.context.bidderDone || [];
-        this.context.bidderDone.push(args);
-        break;
-      
-      case EVENTS.BID_WON:
-        this.context.winningBid = args;
-        break;
+        case EVENTS.BIDDER_DONE:
+          logInfo('HOLID_BIDDER_DONE:', args);
+          if (this.context[args.auctionId]) {
+            this.context[args.auctionId].bidderDone.push(args);
+          }
+          break;
+        
+        case EVENTS.BID_WON:
+          logInfo('HOLID_BID_WON:', args);
+          if (this.context[args.auctionId]) {
+            this.context[args.auctionId].winningBids.push(args);
+          }
+          break;
 
-      case EVENTS.AUCTION_END:
-        this.sendAnalytics();
-        break;
+        case EVENTS.AD_RENDER_FAILED:
+          if (args?.bid?.auctionId && this.context[args.bid.auctionId]) {
+            this.context[args.bid.auctionId].adRenderFailed.push(args);
+          }
+          break;
 
-      case EVENTS.BID_TIMEOUT:
-        this.context.bidTimeout = args;
-        break;
+        case EVENTS.BID_TIMEOUT:
+          logInfo('HOLID_BID_TIMEOUT:', args);
+          (args || []).forEach(timeoutBid => {
+            if (timeoutBid.auctionId && this.context[timeoutBid.auctionId]) {
+              this.context[timeoutBid.auctionId].bidTimeout.push(timeoutBid);
+            }
+          });
+          break;
+
+        case EVENTS.AUCTION_END:
+          logInfo('HOLID_AUCTION_END:', args);
+          if (this.context[args.auctionId]) {
+            // Set a delay, as BID_WON events will come after AUCTION_END events
+            setTimeout(() => holidAnalytics.sendAnalyticsEvents(args.auctionId), 1000);
+          }
+          break;
+      }
+  }    
+});
+
+// save the base class function
+holidAnalytics.originEnableAnalytics = holidAnalytics.enableAnalytics;
+
+// override enableAnalytics so we can get access to the config passed in from the page
+holidAnalytics.enableAnalytics = function (config) {
+  holidAnalytics.originEnableAnalytics(config);
+}
+
+holidAnalytics.sendAnalyticsEvents = function(auctionId) {
+  const auction = this.context[auctionId];
+  if (!auction) return;
+
+  let gdprApplies = auction.bidRequests[0].gdprConsent ? auction.bidRequests[0].gdprConsent.gdprApplies : undefined;
+  let gdprConsent = auction.bidRequests[0].gdprConsent ? auction.bidRequests[0].gdprConsent.consentString : undefined;
+
+  if (gdprApplies === true && !gdprConsent) {
+    logInfo('Holid Analytics: GDPR applies but no consent string found. Skipping analytics.');
+    return;
+  }
+
+  const adUnitCodes = [];
+  auction.bidRequests.forEach(req => {
+    req.bids.forEach(bid => {
+      if (!adUnitCodes.includes(bid.adUnitCode)) {
+        adUnitCodes.push(bid.adUnitCode);
+      }
+    });
+  });
+
+  // Wait until all adUnitCode divs have iframes rendered inside
+  const maxWaitTime = 1000; // in ms
+  const intervalTime = 100;
+  let elapsed = 0;
+
+  const waitForDOMReady = () => {
+    const ready = adUnitCodes.every(adUnitCode => {
+      const outerDiv = document.getElementById(adUnitCode);
+      return outerDiv && outerDiv.querySelector("div[id^='google_ads_iframe_']");
+    });
+
+    if (ready || elapsed >= maxWaitTime) {
+      enrichWithPlacementNames(); // extract placement names
+      sendPayload();              // send data
+    } else {
+      elapsed += intervalTime;
+      setTimeout(waitForDOMReady, intervalTime);
     }
-  },
-  
-  sendAnalytics() {
+  };
+
+  const enrichWithPlacementNames = () => {
+    auction.bidRequests.forEach(bidRequest => {
+      bidRequest.bids.forEach(bid => {
+        const adUnitCode = bid.adUnitCode;
+        const outerDiv = document.getElementById(adUnitCode);
+
+        if (outerDiv) {
+          const childDiv = outerDiv.querySelector("div[id^='google_ads_iframe_']");
+          if (childDiv && childDiv.id) {
+            const match = childDiv.id.match(/\/21756427176\/(.+?)_0__container__/);
+            if (match && match[1]) {
+              const placementName = match[1];
+              bid.placementName = placementName;
+              logInfo(`Holid Analytics: Placement Name for ${adUnitCode}:`, placementName);
+            } else {
+              logInfo(`Holid Analytics: No placement name matched for ${adUnitCode}.`);
+            }
+          } else {
+            logInfo(`Holid Analytics: No matching child div found in ${adUnitCode}.`);
+          }
+        } else {
+          logInfo(`Holid Analytics: Outer div not found for ${adUnitCode}.`);
+        }
+      });
+    });
+  }
+
+  const sendPayload = () => {
     const payload = {
-      auctionId: this.context.auctionId || null,
-      startTime: this.context.startTime || null,
-      bidRequests: this.context.bidRequests || [],
-      bidResponses: this.context.bidResponses || [],
-      bidRejected: this.context.bidRejected || [],
-      bidderDone: this.context.bidderDone || [],
-      winningBid: this.context.winningBid || null,
-      bidTimeout: this.context.bidTimeout || [],
+      auctionId: auction.auctionId,
+      startTime: auction.startTime,
+      bidRequests: auction.bidRequests,
+      bidResponses: auction.bidResponses,
+      bidRejected: auction.bidRejected,
+      bidderDone: auction.bidderDone,
+      bidTimeout: auction.bidTimeout,
+      winningBids: auction.winningBids,
+      adRenderFailed: auction.adRenderFailed,
       userAgent: navigator.userAgent,
       screen: {
         width: screen.width,
         height: screen.height
       }
-      // Optional: add more like viewability, IP hash (if collected), etc.
-    };
-
-    ajax(ANALYTICS_URL, null, JSON.stringify(payload), {
-      method: 'POST', 
-      contentType: 'application/json'
+    }
+  
+    ajax(ANALYTICS_URL + END_POINT, null, JSON.stringify(payload), {
+      method: 'POST'
     });
 
     logInfo('Holid Analytics: sent payload', payload);
   }
-});
+
+  waitForDOMReady(); // start polling
+}
 
 adapterManager.registerAnalyticsAdapter({
   adapter: holidAnalytics,
   code: 'holid'
 });
+
+holidAnalytics.enableAnalytics({}); // auto enable
 
 export default holidAnalytics;
